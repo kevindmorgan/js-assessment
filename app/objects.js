c1dba3b631bb461a6ec514e0e66ef6fd21bce7f0
Kevin Morgan <kevinm@potatolondon.com>
2013-01-27 15:23:36 +0000

    Completing object functions.

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+        return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+        constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+        var properties = [];
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                properties.push(
+                    [prop, ': ', obj[prop]].join('')
+                );
+            }
+        }
+        return properties;
     }
   };
 });
